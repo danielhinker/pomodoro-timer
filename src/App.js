3c087a569943f00f9fde085c1b1695228e976d3a
Daniel Hinker <danhinker@gmail.com>
2019-08-29 20:30:03 -0400

    Added updated files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,258 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      break: 5,
+      sessionLength: 25,
+      timeLeft: 25,
+      isTimerOn: false,
+      isBreak: false,
+      seconds: '00',
+      minutes: '00'
+    };
+
+    
+
+    this.handleBreak = this.handleBreak.bind(this);
+    this.handleSession = this.handleSession.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.handleStart = this.handleStart.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+    this.handleIntervals = this.handleIntervals.bind(this);
+    this.handleBreakInterval = this.handleBreakInterval.bind(this);
+    this.handleBeep = this.handleBeep.bind(this);
+    this.setTimer = this.setTimer.bind(this);
+    this.setBreakTimer = this.setBreakTimer.bind(this);
+  }
+
+  handleBeep = (e) => {
+    const beep = document.getElementById("beep");
+    beep.play();
+  }
+
+  handleBreak = (e) => {
+    e.persist();
+
+    if (e.target.id === "break-increment" && this.state.break < 60) {
+      this.setState((prevState) => ({
+        break: prevState.break += 1
+      }));
+    } else if (e.target.id === "break-decrement" && this.state.break > 1) {
+      this.setState((prevState) => ({
+        break: prevState.break -= 1
+      }));
+    }
+  }
+
+  handleSession = (e) => {
+    e.persist();
+
+    if (e.target.id === "session-increment" && this.state.sessionLength < 60) {
+      this.setState((prevState) => ({
+        sessionLength: prevState.sessionLength += 1,
+        timeLeft: prevState.sessionLength += 1
+      }));
+    } else if (e.target.id === "session-decrement" && this.state.sessionLength > 1) {
+      this.setState((prevState) => ({
+        sessionLength: prevState.sessionLength -= 1,
+        timeLeft: prevState.sessionLength -= 1
+      }));
+    }
+  }
+
+  setTimer = () => {
+  
+    this.secondsRemaining = this.state.sessionLength * 60
+    this.timerInterval = setInterval(this.handleIntervals, 1000);
+  }
+
+  setBreakTimer = () => {
+    
+    this.secondsRemaining = this.state.break * 60
+    this.timerBreakInterval = setInterval(this.handleBreakInterval, 1000);
+  }
+
+  handleBreakInterval = () => {
+    let min = Math.floor(this.secondsRemaining / 60);
+    let sec = this.secondsRemaining - (min * 60);
+
+    this.setState((prevState) => ({
+      minutes: min,
+      seconds: sec
+    }));
+    if (sec < 10) {
+      this.setState({
+        seconds: "0" + this.state.seconds
+      })
+    }
+    if (min < 10) {
+      this.setState({
+        minutes: "0" + this.state.minutes,
+      })
+    }
+    if (min === 0 & sec === 0) {
+      this.handleBeep();
+      clearInterval(this.timerBreakInterval);
+
+      this.setState({
+        isBreak: false,
+        isTimerOn: true,
+        // minutes: min,
+        // seconds: sec
+      })
+
+      this.setTimer()
+      this.timerInterval = setInterval(this.handleIntervals, 1000);
+    }
+
+    this.secondsRemaining--
+
+  }
+
+  handleIntervals = () => {
+
+    let min = Math.floor(this.secondsRemaining / 60);
+    let sec = this.secondsRemaining - (min * 60);
+    
+    this.setState((prevState) => ({
+      minutes: min,
+      seconds: sec
+    }));
+    if (sec < 10) {
+      this.setState({
+        seconds: "0" + this.state.seconds
+      })
+    }
+    if (min < 10) {
+      this.setState({
+        minutes: "0" + this.state.minutes,
+      })
+    }
+    if (min === 0 & sec === 0) {
+      this.handleBeep();
+      clearInterval(this.timerInterval);
+
+      this.setState({
+        isBreak: true,
+        isTimerOn: true,
+        // minutes: min,
+        // seconds: sec
+      })
+      
+     this.setBreakTimer()
+      this.timerBreakInterval = setInterval(this.handleBreakInterval, 1000);
+    }
+
+    this.secondsRemaining--
+
+  }
+
+  handleStart = (e) => {
+    e.persist();
+
+    this.setState((prevState) => ({
+      isTimerOn: true,
+      isBreak: false
+    }));
+
+    this.setTimer()
+  }
+
+  handleStop = (e) => {
+    e.persist();
+
+    this.setState((prevState) => ({
+      isTimerOn: false
+    }));
+    clearInterval(this.timerInterval)
+    // clearInterval(this.timerBreakInterval)
+
+  }
+
+  handleReset = (e) => {
+    e.persist();
+    const beep = document.getElementById("beep");
+    beep.pause()
+    beep.currentTime = 0;
+    this.setState((prevState) => ({
+      break: 5,
+      sessionLength: 25,
+      timeLeft: 25,
+      seconds: '00',
+      minutes: '00',
+      isTimerOn: false
+    }));
+    clearInterval(this.timerInterval)
+    // clearInterval(this.timerBreakInterval)
+  }
+
+  render() {
+
+    let button;
+    let breakIncrement;
+    let breakDecrement;
+    let sessionIncrement;
+    let sessionDecrement;
+    let timeLeft;
+    let timerLabel;
+
+    if (this.state.isTimerOn) {
+      button = <button id="start_stop" onClick={this.handleStop}>Stop</button>
+      breakIncrement = <button id="break-increment">Increase</button>
+      breakDecrement = <button id="break-decrement">Decrease</button>
+      sessionIncrement = <button id="session-increment">Increase</button>
+      sessionDecrement = <button id="session-decrement">Decrease</button>
+      timeLeft = <div id="time-left">{this.state.minutes}:{this.state.seconds}</div>
+    } else {
+      button = <button id="start_stop" onClick={this.handleStart}>Start</button>
+      breakIncrement = <button id="break-increment" onClick={this.handleBreak}>Increase</button>
+      breakDecrement = <button id="break-decrement" onClick={this.handleBreak}>Decrease</button>
+      sessionIncrement = <button id="session-increment" onClick={this.handleSession}>Increase</button>
+      sessionDecrement = <button id="session-decrement" onClick={this.handleSession}>Decrease</button>
+      timeLeft = <div id="time-left">{this.state.sessionLength}:00</div>
+    }
+
+    if (this.state.isBreak) {
+      timerLabel = <h1 id="timer-label">Break</h1>
+    } else {
+      timerLabel = <h1 id="timer-label">Session</h1>
+    }
+
+    return (
+      <div>
+        <h1 id="break-label">Break Length</h1>
+        {/* <button id="break-increment" onClick={this.handleBreak}>Increase</button> */}
+        {breakIncrement}
+        <div id="break-length">{this.state.break}</div>
+        {/* <button id="break-decrement" onClick={this.handleBreak}>Decrease</button> */}
+        {breakDecrement}
+
+        <h1 id="session-label">Session Length</h1>
+        {/* <button id="session-increment" onClick={this.handleSession}>Increase</button> */}
+        {sessionIncrement}
+        <div id="session-length">{this.state.sessionLength}</div>
+        {/* <button id="session-decrement" onClick={this.handleSession}>Decrease</button> */}
+        {sessionDecrement}
+
+        {timerLabel}
+        {/* <div id="time-left">{this.state.minutes}:{this.state.seconds}</div> */}
+        {timeLeft}
+
+
+        {/* <button id="start_stop" onClick={this.handleStart}>Start/Stop</button> */}
+        {button}
+
+        <button id="reset" onClick={this.handleReset}>Reset</button>
+
+        <audio className="clip" id="beep" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/success.mp3" />
+      </div>
+    );
+  }
 }
 
 export default App;
